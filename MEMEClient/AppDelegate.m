97e6ce001bb565ccedaa7fada1034c34292e4e0e
kenzan8000 <kenzan8000@gmail.com>
2015-12-28 15:21:37 -0800

    add user interface for text

--- MEMEClient/AppDelegate.m
+++ MEMEClient/AppDelegate.m
@@ -3,35 +3,72 @@
 #import "MEMELib.h"
 
 
+#pragma mark - interface
 @interface AppDelegate () <MEMELibDelegate>
 
+
+#pragma mark - properties
+
 @property (weak) IBOutlet NSWindow *window;
-@property (nonatomic) NSButton *APITestButton;
-@property (nonatomic) NSButton *serverButton;
+@property (nonatomic) NSMutableArray *buttons;
+@property (nonatomic) NSTextView *textView;
+@property (nonatomic) NSPopUpButton *popupButton;
+
+@property (nonatomic) NSMutableArray *peripherals;
 
 @end
 
 
+#pragma mark - AppDelegate
 @implementation AppDelegate
+
+
+#pragma mark - destruction
+- (void)dealloc
+{
+}
 
 
 #pragma mark - life cycle
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    self.APITestButton = [[NSButton alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
-    self.APITestButton.title = @"test api";
-    [self.APITestButton setTarget:self];
-    [self.APITestButton setAction:@selector(clickedWithButton:)];
-    [self.window.contentView addSubview:self.APITestButton];
-    
-    self.serverButton = [[NSButton alloc] initWithFrame:CGRectMake(1000, 0, 100, 100)];
-    self.serverButton.title = @"server";
-    [self.serverButton setTarget:self];
-    [self.serverButton setAction:@selector(clickedWithButton:)];
-    [self.window.contentView addSubview:self.serverButton];
+    self.peripherals = @[].mutableCopy;
 
-    [MEMELib setAppClientId:@"YOUR_MEME_APP_CLIENT_ID"
-               clientSecret:@"YOUR_MEME_CLIENT_SECRET"];
-    [[MEMELib sharedInstance] setDelegate:self];
+    // button
+    NSArray *APIs = @[
+        @"setAppClientId",
+        @"isConnected",
+        @"isDataReceiving",
+        @"isCalibrated",
+        @"startScanningPeripherals",
+        @"stopScanningPeripherals",
+        @"connectPeripheral",
+        @"disconnectPeripheral",
+        @"getConnectedByOthers",
+        @"startDataReport",
+        @"stopDataReport",
+        @"getSDKVersion",
+        @"getFWVersion",
+        @"getHWVersion",
+        @"getConnectedDeviceType",
+        @"getConnectedDeviceSubType",
+    ];
+    self.buttons = @[].mutableCopy;
+    for (int i = 0; i < APIs.count; i++) {
+        NSButton *button = [[NSButton alloc] initWithFrame:CGRectMake(0, 20*i, 200, 20)];
+        button.title = APIs[i];
+        [button setTarget:self];
+        [button setAction:@selector(clickedWithButton:)];
+        [self.window.contentView addSubview:button];
+    }
+
+    // textview
+    self.textView = [[NSTextView alloc] initWithFrame:CGRectMake(200, 0, 320, 320)];
+    [self.window.contentView addSubview:self.textView];
+
+    // popup button
+    self.popupButton = [[NSPopUpButton alloc] initWithFrame:CGRectMake(200, 320, 200, 20)];
+    [self.window.contentView addSubview:self.popupButton];
+    [self.popupButton selectItemAtIndex:0];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
@@ -41,8 +78,71 @@
 #pragma mark - event listener
 - (void)clickedWithButton:(NSButton *)button
 {
-    if (button == self.APITestButton) {
-        [self testAPIs];
+    NSString *title = [button title];
+    if ([title isEqualToString:@"setAppClientId"]) {
+        [MEMELib setAppClientId:@"775649328784742"
+                   clientSecret:@"7pir5husrqk4o8zh2z6tdszncpzk7g2w"];
+        [[MEMELib sharedInstance] setDelegate:self];
+
+        [self.textView setString:@"[MEMELib setAppClientId:@\"775649328784742\" clientSecret:@\"7pir5husrqk4o8zh2z6tdszncpzk7g2w\"]"];
+    }
+    else if ([title isEqualToString:@"isConnected"]) {
+        [self.textView setString:[NSString stringWithFormat:@"[[MEMELib sharedInstance] isConnected] = %d", [[MEMELib sharedInstance] isConnected]]];
+    }
+    else if ([title isEqualToString:@"isDataReceiving"]) {
+        [self.textView setString:[NSString stringWithFormat:@"[[MEMELib sharedInstance] isDataReceiving] = %d", [[MEMELib sharedInstance] isDataReceiving]]];
+    }
+    else if ([title isEqualToString:@"isCalibrated"]) {
+        [self.textView setString:[NSString stringWithFormat:@"[[MEMELib sharedInstance] isCalibrated] = %d", [[MEMELib sharedInstance] isCalibrated]]];
+    }
+    else if ([title isEqualToString:@"startScanningPeripherals"]) {
+        [self.textView setString:[NSString stringWithFormat:@"[[MEMELib sharedInstance] startScanningPeripherals] = %d", [[MEMELib sharedInstance] startScanningPeripherals]]];
+    }
+    else if ([title isEqualToString:@"stopScanningPeripherals"]) {
+        [self.textView setString:[NSString stringWithFormat:@"[[MEMELib sharedInstance] stopScanningPeripherals] = %d", [[MEMELib sharedInstance] stopScanningPeripherals]]];
+    }
+    else if ([title isEqualToString:@"connectPeripheral"]) {
+        NSString *UUIDString = [self.popupButton itemTitleAtIndex:[self.popupButton indexOfSelectedItem]];
+        if (UUIDString == nil) {
+            [self.textView setString:@"[[MEMELib sharedInstance] connectPeripheral:] failed because of no selected peripherals"];
+            return;
+        }
+
+        CBPeripheral *peripheral = [CBPeripheral new];
+        peripheral.identifier = [[NSUUID alloc] initWithUUIDString:UUIDString];
+        [self.textView setString:[NSString stringWithFormat:@"[[MEMELib sharedInstance] connectPeripheral:peripheral] = %d", [[MEMELib sharedInstance] connectPeripheral:peripheral]]];
+    }
+    else if ([title isEqualToString:@"disconnectPeripheral"]) {
+        [self.textView setString:[NSString stringWithFormat:@"[[MEMELib sharedInstance] disconnectPeripheral] = %d", [[MEMELib sharedInstance] disconnectPeripheral]]];
+    }
+    else if ([title isEqualToString:@"getConnectedByOthers"]) {
+        NSArray *ps = [[MEMELib sharedInstance] getConnectedByOthers];
+        NSString *text = [NSString stringWithFormat:@"[[MEMELib sharedInstance] getConnectedByOthers]\n"];
+        for (int i = 0; i < [ps count]; i++) {
+            text = [NSString stringWithFormat:@"%@UUID %d: %@\n", text, i, [[ps[i] identifier] UUIDString]];
+        }
+        [self.textView setString:text];
+    }
+    else if ([title isEqualToString:@"startDataReport"]) {
+        [self.textView setString:[NSString stringWithFormat:@"[[MEMELib sharedInstance] startDataReport] = %d", [[MEMELib sharedInstance] startDataReport]]];
+    }
+    else if ([title isEqualToString:@"stopDataReport"]) {
+        [self.textView setString:[NSString stringWithFormat:@"[[MEMELib sharedInstance] stopDataReport] = %d", [[MEMELib sharedInstance] stopDataReport]]];
+    }
+    else if ([title isEqualToString:@"getSDKVersion"]) {
+        [self.textView setString:[NSString stringWithFormat:@"[[MEMELib sharedInstance] getSDKVersion] = %@", [[MEMELib sharedInstance] getSDKVersion]]];
+    }
+    else if ([title isEqualToString:@"getFWVersion"]) {
+        [self.textView setString:[NSString stringWithFormat:@"[[MEMELib sharedInstance] getFWVersion] = %@", [[MEMELib sharedInstance] getFWVersion]]];
+    }
+    else if ([title isEqualToString:@"getHWVersion"]) {
+        [self.textView setString:[NSString stringWithFormat:@"[[MEMELib sharedInstance] getHWVersion] = %d", [[MEMELib sharedInstance] getHWVersion]]];
+    }
+    else if ([title isEqualToString:@"getConnectedDeviceType"]) {
+        [self.textView setString:[NSString stringWithFormat:@"[[MEMELib sharedInstance] getConnectedDeviceType] = %d", [[MEMELib sharedInstance] getConnectedDeviceType]]];
+    }
+    else if ([title isEqualToString:@"getConnectedDeviceSubType"]) {
+        [self.textView setString:[NSString stringWithFormat:@"[[MEMELib sharedInstance] getConnectedDeviceSubType] = %d", [[MEMELib sharedInstance] getConnectedDeviceSubType]]];
     }
 }
 
@@ -60,7 +160,17 @@
 
 - (void) memePeripheralFound: (CBPeripheral *) peripheral withDeviceAddress: (NSString *) address
 {
-    NSLog(@"UUIDString: %@\naddress: %@", [peripheral.identifier UUIDString], address);
+    NSString *UUIDString = [peripheral.identifier UUIDString];
+    NSLog(@"UUIDString: %@\naddress: %@", UUIDString, address);
+
+    BOOL alreadyFound = FALSE;
+    for (CBPeripheral *peripheral in self.peripherals) {
+        if ([[peripheral.identifier UUIDString] isEqualToString:UUIDString]) { alreadyFound = TRUE; break; }
+    }
+    if (alreadyFound) { return; }
+    [self.peripherals addObject:peripheral];
+
+    [self.popupButton addItemWithTitle:UUIDString];
 }
 
 - (void) memePeripheralConnected: (CBPeripheral *)peripheral
@@ -102,12 +212,13 @@
 
 
 #pragma mark - private api
+/*
 - (void)testAPIs
 {
     NSLog(@"///////////////////////////////////////////////////////");
     NSLog(@"Calling MEMELib APIs");
-    // APIs
 
+    // APIs
     CBPeripheral *peripheral = [CBPeripheral new];
     //peripheral.identifier = [[NSUUID alloc] initWithUUIDString:@"PERIPHERAL_UUID_STRING"];
     peripheral.identifier = [NSUUID new];
@@ -115,7 +226,6 @@
     NSLog(@"[[MEMELib sharedInstance] isConnected]        %d", [[MEMELib sharedInstance] isConnected]);
     NSLog(@"[[MEMELib sharedInstance] isDataReceiving]    %d", [[MEMELib sharedInstance] isDataReceiving]);
     NSLog(@"[[MEMELib sharedInstance] isCalibrated]       %d", [[MEMELib sharedInstance] isCalibrated]);
-
     NSLog(@"[[MEMELib sharedInstance] startScanningPeripherals]       %d", [[MEMELib sharedInstance] startScanningPeripherals]);
     NSLog(@"[[MEMELib sharedInstance] stopScanningPeripherals]        %d", [[MEMELib sharedInstance] stopScanningPeripherals]);
     NSLog(@"[[MEMELib sharedInstance] connectPeripheral:peripheral]   %d", [[MEMELib sharedInstance] connectPeripheral:peripheral]);
@@ -136,5 +246,7 @@
     NSLog(@"[[MEMELib sharedInstance] getConnectedDeviceType]       %d", [[MEMELib sharedInstance] getConnectedDeviceType]);
     NSLog(@"[[MEMELib sharedInstance] getConnectedDeviceSubType]    %d", [[MEMELib sharedInstance] getConnectedDeviceSubType]);
 }
+*/
+
 
 @end
